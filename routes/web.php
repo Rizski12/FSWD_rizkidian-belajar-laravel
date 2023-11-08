eaf132be8806f6876874d7520e97319fcd21c2a3
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-11-08 15:12:13 +0700

    [FIX] Memperbarui routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\BiodataController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/biodata', [BiodataController::class, 'ShowBiodata']);
+Route::get('/biodata', [BiodataController::class, 'showBiodata'])->name('biodata');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/profile', [HomeController::class, 'profile'])->name('profile');
+
+Route::get('/products', [ProductController::class, 'products'])->name('products');
