b37b2d901d35bd564b304072e26457172756ea74
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-11-11 16:36:56 +0700

    [FIX] Memperbaruhi routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BiodataController;
+use App\Http\Controllers\CrudProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 
@@ -28,3 +29,10 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/profile', [HomeController::class, 'profile'])->name('profile');
 
 Route::get('/products', [ProductController::class, 'products'])->name('products');
+
+Route::get('/crud', [CrudProductController::class, 'index'])->name('crud.index');
+Route::get('/crud/create', [CrudProductController::class, 'create'])->name('crud.create');
+Route::post('/crud', [CrudProductController::class, 'store'])->name('crud.store');
+Route::get('/crud/{product}/edit', [CrudProductController::class, 'edit'])->name('crud.edit');
+Route::put('/crud/{product}', [CrudProductController::class, 'update'])->name('crud.update');
+Route::delete('/crud/{product}', [CrudProductController::class, 'destroy'])->name('crud.destroy');
