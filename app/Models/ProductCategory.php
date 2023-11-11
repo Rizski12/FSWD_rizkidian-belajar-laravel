7148851352f1c99ae2986f7f2808fd996f6c7185
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-11-11 16:36:20 +0700

    [FEAT] Product Category Models

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_name',
+        'created_by',
+        'updated_by',
+        'is_active',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id');
+    }
+}
