ba3ced430b2fa521a85154c32ebf5960390142aa
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-11-11 16:37:18 +0700

    Memperbaiki code

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('dist/img/AdminLTELogo.png')  }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -58,6 +58,15 @@
                 <i class="nav-icon fas fa-box"></i>
                 <p>
                   Product
+                  <i class="fas fa-angle-right right"></i>
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ route('crud.index') }}" class="nav-link">
+                <i class="nav-icon fas fa-cube"></i>
+                <p>
+                  Crud Product
                   <i class="fas fa-angle-right right"></i>
                 </p>
               </a>
