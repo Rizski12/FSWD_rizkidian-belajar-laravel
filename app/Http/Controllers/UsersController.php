7c973fa463968649cf80f6821a53d2409db910c2
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-11-28 16:49:29 +0700

    Users Controller

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Users;
+use App\Models\UserGroups;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = Users::with('group')->get();
+        return view('users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        $userGroups = UserGroups::pluck('group_name', 'id');
+        return view('users.create', compact('userGroups'));
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'phone_number' => 'required',
+            'username' => 'required|unique:users',
+            'password' => 'required|min:8',
+            'group_id' => 'required|exists:user_groups,id', // Validation for group_id
+            'is_active' => 'required',
+            // Add other validation rules as needed
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        Users::create($validatedData);
+
+        return redirect()->route('users.index')->with('success', 'User created successfully');
+    }
+
+    public function edit(Users $user)
+    {
+        $userGroups = UserGroups::pluck('group_name', 'id');
+        return view('users.edit', compact('user', 'userGroups'));
+    }
+
+    public function update(Request $request, Users $user)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            'phone_number' => 'required',
+            'username' => 'required|unique:users,username,' . $user->id,
+            'group_id' => 'required|exists:user_groups,id', // Validation for group_id
+            'is_active' => 'required',
+            // Add other validation rules as needed
+        ]);
+
+        $user->update($validatedData);
+
+        return redirect()->route('users.index')->with('success', 'User updated successfully');
+    }
+
+    public function destroy(Users $user)
+    {
+        $user->delete();
+
+        return redirect()->route('users.index')->with('success', 'User deleted successfully');    }
+}
