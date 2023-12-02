7cfebabc2d294f483284905d7e58e4d159fcbcef
Rizki Dian Pratama <rizkidianpratama784@gmail.com>
2023-12-02 16:23:18 +0700

    halaman unauthorized

---
+++ resources/views/pages/unauthorized.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('konten')
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center bg-danger text-white">Halaman Users Tidak Dapat di Akses</div>
+
+                <div class="card-body">
+                    <p class="text-center">Hanya Super Admin yang dapat mengakses halaman ini!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
